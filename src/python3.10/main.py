44b3fc501824b712b1364ab851d6fbdb132dffc4
Joe Barrows <joebarrows.dev@gmail.com>
2023-07-10 14:35:06 -0400

    updating an api call and printing for test

--- src/python3.10/main.py
+++ src/python3.10/main.py
@@ -1,15 +1,49 @@
+import requests
+import random
 from prefect import flow, task
+from prefect.artifacts import create_markdown_artifact
 
 @task
-def create_message():
-    msg = "this is my test task"
-    return msg
+def get_data(url):
+    try:
+        # Send a GET request to the API endpoint
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        response.raise_for_status()
+
+        # Access the response content in JSON format
+        data = response.json()
+
+    except e:
+        print(f"Error: {e}")
+
+    return data
+
+@task
+def update_data(data):
+    for record in data:
+        record['update'] = int((random.random() * 100))
+    return data
+
+@task
+def print_data(data):
+    for record in data:
+        print(f"Post ID: {record['id']}")
+        print(f"Title: {record['title']}")
+        print(f"Body: {record['body']}")
+        print(f"This is the UPDATE: {record['update']}")
+        print('---')
 
 @flow
-def test_flow():
-    task_message = create_message()
-    print(task_message)
+def main():
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    data = get_data(url)
+    if data:
+        data = update_data(data)
+        print_data(data)
 
 
-if __name__ == "__main__":
-    test_flow()
+if __name__ == '__main__':
+    main()
+
