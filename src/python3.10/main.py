4d0489be77325ef19304210f78e2e192ee1d942c
Joe Barrows <joebarrows.dev@gmail.com>
2023-07-10 14:56:16 -0400

    working, now updating print to creating markdown

--- src/python3.10/main.py
+++ src/python3.10/main.py
@@ -1,48 +1,46 @@
 import requests
-import random
+import uuid
 from prefect import flow, task
 from prefect.artifacts import create_markdown_artifact
 
 @task
 def get_data(url):
     try:
-        # Send a GET request to the API endpoint
         response = requests.get(url)
-
-        # Check if the request was successful (status code 200)
         response.raise_for_status()
-
-        # Access the response content in JSON format
         data = response.json()
-
-    except e:
-        print(f"Error: {e}")
-
+    except Exception as e:
+        raise e
     return data
 
 @task
 def update_data(data):
+    desired_fields = ['PatientId', 'PatientName', 'Collection']
+    updated_record = []
     for record in data:
-        record['update'] = int((random.random() * 100))
-    return data
+        new_dict = {key: record[key] for key in desired_fields}
+        new_dict['PatientId'] = str(uuid.uuid4())
+        updated_record.append(new_dict)
+    return updated_record
 
 @task
 def print_data(data):
     for record in data:
-        print(f"Post ID: {record['id']}")
-        print(f"Title: {record['title']}")
-        print(f"Body: {record['body']}")
-        print(f"This is the UPDATE: {record['update']}")
-        print('---')
+        print(record)
 
 @flow
 def main():
-    url = 'https://jsonplaceholder.typicode.com/posts'
-    data = get_data(url)
-    if data:
-        data = update_data(data)
-        print_data(data)
-
+    request_date = '2010/08/16'
+    url = f"https://services.cancerimagingarchive.net/nbia-api/services/v1/NewPatientsInCollection?Collection=CBIS-DDSM&Date={request_date}"
+    try:
+        data = get_data(url)
+        if data:
+            data = update_data(data)
+            print_data(data)
+        else:
+            print(f"No data to process for date: {request_date}")
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == '__main__':
     main()
